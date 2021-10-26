0e9af1779a8ccdbdb6d19c8e953a621ed0ae41f6
daniel.kurushin@gmail.com <daniel.kurushin@gmail.com>
2021-10-26 12:46:28 +0500

    CLI интерфейс тут

--- main.py
+++ main.py
@@ -1,48 +1,49 @@
-from tkinter import *
-
+from matplotlib.path import Path
 from itertools import product
 from numpy import polyfit
 from random import randint
 
+poly = [
+    (0,0),
+    (4,0),
+    (1,1),
+    (4,3),
+    (0,3),
+    (0,0),
+]
+
 poly = [ (randint(-7,12), randint(-7,12)) for i in range(randint(3, 10)) ]
 poly += [poly[0]]
-
-def c_to_canvas(a):
-    return (a + 7) * 25
-
-def create_interface():
-    root = Tk()
-    root.geometry("500x520")
-    canvas = Canvas(root, bg='white')
-    message = StringVar(root)
-    entry = Entry(root, textvariable=message)
-    canvas.pack(expand=1, fill=BOTH)
-    entry.pack(side=BOTTOM)
-    
-    
-    for i in range(20):
-        x, y = i*25, i*25
-        canvas.create_line(0, y, 500, y, fill='gray')
-        canvas.create_line(x, 0, x, 500, fill='gray')
-        
-    for x, y in poly:
-        x1, y1, x2, y2 = [ c_to_canvas(x) for x in [x-0.1, y-0.1, x+0.1, y+0.1] ]
-        canvas.create_oval(x1, y1, x2, y2)
-
-    for a, b in [ poly[i:i+2] for i in range(len(poly)-1) ]:
-        x1, y1, x2, y2 = [ c_to_canvas(x) for x in a + b ]
-        canvas.create_line(x1, y1, x2, y2, fill='blue', width=2)
-    
-    root.bind('<Escape>', lambda x: root.destroy())
-    return root, canvas, message
-
-def draw_points(canvas, A_points, B_points):
-    for x, y in B_points:
-        x1, y1, x2, y2 = [ c_to_canvas(x) for x in [x-0.1, y-0.1, x+0.1, y+0.1] ]
-        canvas.create_oval(x1, y1, x2, y2, fill='green')
-    for x, y in A_points:
-        x1, y1, x2, y2 = [ c_to_canvas(x) for x in [x-0.1, y-0.1, x+0.1, y+0.1] ]
-        canvas.create_oval(x1, y1, x2, y2, fill='red')
+#poly = [
+#    (0,0),
+#    (4,0),
+#    (1,1),
+#    (4,3),
+#    (0,3),
+#    (0,0),
+#]
+#
+poly = [
+    (0,0),
+    (0,5),
+    (7,5),
+    (5,2),
+    (3,4),
+    (1,2),
+    (1,1),
+    (3,3),
+    (5,2),
+    (7,4),
+    (7,0),
+    (0,0),
+]
+#poly = [
+#    (0, 3),
+#    (4, 5),
+#    (4, 3),
+#    (1, 0),
+#    (0, 3),
+#]
 
 def point_in_poly(p, poly):
     x, y = p
@@ -58,6 +59,7 @@ def point_in_poly(p, poly):
     return odd
 
 def find_A_points(poly, B_points):
+    
     min_x, max_x = min([x[0] for x in poly ]), max([x[0] for x in poly ])
     min_y, max_y = min([x[1] for x in poly ]), max([x[1] for x in poly ])
     all_points = set(product(range(min_x+1, max_x), range(min_y+1, max_y))) - B_points
@@ -81,6 +83,7 @@ def find_B_points(poly):
             min_y, max_y = min(Y), max(Y)+1
             x = X[0]
             B_points |= set([ (x, y) for y in range(min_y, max_y) ])
+            
     return B_points
 
 B_points = find_B_points(poly)
@@ -89,8 +92,6 @@ A = len(A_points)
 B = len(B_points)
 S = A + B / 2 - 1
 
-root, canvas, message = create_interface()
-draw_points(canvas, A_points, B_points)
-message.set("%s = %s + %s / 2 - 1" % (S, A, B))
-root.mainloop()
+print("%s = %s + %s / 2 - 1" % (S, A, B))
+
 
